2e837ba5cd7f37fefb6762e301996764b06fe9c5
ySoares <79868726+Soares007@users.noreply.github.com>
2023-05-06 22:54:11 -0300

    Update pokemon.ts
    Abilities and types added

--- src/app/pokemon.ts
+++ src/app/pokemon.ts
@@ -1,15 +1,15 @@
 export interface Pokemon {
   name: string;
-    id: number;
-    height: number;
-    weight: number;
-    sprites: {
-        other: {
-          home: {
-            front_default: string;
-          }
-        }
+  id: number;
+  height: number;
+  weight: number;
+  sprites: {
+    other: {
+      home: {
+        front_default: string;
+      }
     }
+  }
   base_experience: number;
   stats: {
     base_stat: number;
@@ -17,4 +17,16 @@ export interface Pokemon {
       name: string;
     };
   }[];
-  }
+  abilities: {
+    ability: {
+      name: string;
+    };
+    slot: number;
+  }[];
+  types: {
+    slot: number;
+    type: {
+      name: string;
+    };
+  }[];
+}
